07ce748ae452eca62b44487af42b640ae1bc61a2
Nithin Peter <nithinpeter7@gmail.com>
2020-02-26 23:19:56 +1100

    get git branch

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
-extern crate clap;
-
-use clap::{Arg, App};
+use clap::{App, ArgGroup};
+use std::process::Command;
 
 fn main() {
     let matches = App::new("bb-cli")
-        .about("A CLI app for Bitbucket")
-        .version("0.1.0")
-        .author("npeter@atlassian.com")
         .subcommand(
-            App::new("pr").about("pull requests").arg(
-                Arg::with_name("open")
-                    .long("open")
-                    .short("o")
-                    .help("Open the pull request created from the current branch")
-                    .required(true),
-            ),
+            App::new("pr")
+                .about("Pull requests commands")
+                .args_from_usage(
+                    "--open             'opens the pull request in the browser'
+                            --status           'displays the status of the pull request'
+                            --create           'opens create pull request screen in the browser")
+                .group(ArgGroup::with_name("pr-args")
+                    .args(&["open", "status"])
+                    .required(true))
         )
         .get_matches();
 
+    // Pull request subcommand
+    if let Some(ref matches) = matches.subcommand_matches("pr") {
+        if matches.is_present("open") {
+            // 1. find the current branch name
+            let current_branch= git_current_branch();
+            // 2. check if you have corresponding pull request
 
-    match matches.subcommand() {
-        ("pr", Some(pr_matches)) => {
-            // Now we have a reference to clone's matches
-            println!("Cloning {}", pr_matches.value_of("open").unwrap());
+            // 3. if yes open the pull request
+            println!("can you open that pr??");
         }
-        ("", None) => println!("No subcommand was used"),
-        _ => unreachable!(),
     }
 }
+
+fn git_current_branch() -> String {
+    let result = Command::new("git")
+        .args(&["rev-parse", "--abbrev-ref", "HEAD"])
+        .output()
+        .expect("git command failed to start");
+
+    let branch = String::from_utf8_lossy(&result.stdout);
+    return branch.to_string();
+}
+
+//fn get_pr_link(&current_branch: &str) {
+//
+//}
