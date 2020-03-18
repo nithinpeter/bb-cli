a7bffc0ffff5c2c0e7a49214239d4ffe27fe2b02
Nithin Peter <nithinpeter7@gmail.com>
2020-03-18 23:26:07 +1100

    add the list and status commands

--- src/bitbucket/pr.rs
+++ src/bitbucket/pr.rs
@@ -1,20 +1,23 @@
 extern crate opener;
+extern crate colored;
 
 use url::Url;
 use reqwest::*;
 use serde::{Deserialize, Serialize};
 use std::env;
+use colored::*;
+
 
 pub fn open_pr() {
-    let current_branch = super::git::git_current_branch();
-    if let Some(base_url) = get_api_base_url() {
-        let pr_search_api_url = base_url + "pullrequests?q=source.branch.name~\"" + current_branch.trim() + "\"";
-        let pr_link = get_current_pr(pr_search_api_url).unwrap();
-        opener::open(pr_link);
-    }
+    let pr = get_current_pr().unwrap();
+    let pr_link = pr.links.html.href.clone();
+    opener::open(pr_link);
 }
 
-pub fn status_pr() {}
+pub fn status_pr() {
+    let pr = get_current_pr().unwrap();
+    print_pr(&pr);
+}
 
 pub fn create_pr() {
     let remote_url = super::git::git_remote();
@@ -31,57 +34,127 @@ pub fn create_pr() {
     }
 }
 
+pub fn list_pr() {
+    println!("{}", "Your open pull requests".bold());
+    println!();
+    let prs = get_current_user_prs().unwrap();
+    for pr in prs.iter() {
+        print_pr(pr);
+        println!();
+    }
+}
+
+fn print_pr(pr: &PullRequest) {
+    let pr_link = pr.links.html.href.clone();
+    println!("{}", pr.title.bold().underline());
+    println!("{} -> {}", pr.source.branch.name, pr.destination.branch.name);
+//    println!("{}: {}", "Description".bold(), pr.description);
+    println!("{}: {}", "State".bold(), pr.state);
+    println!("{}  {}", "🔗".bold(), pr_link);
+}
+
+fn print_line() {
+    println!("--------------------------------");
+}
+
 fn get_api_base_url() -> std::option::Option<String> {
     let remote_url = super::git::git_remote();
     if let Ok(parsed) = Url::parse(&remote_url) {
-        let slug = parsed.path().split(".git").collect::<Vec<_>>().join("");
         let username = env::var("BB_CLI_USERNAME").unwrap();
         let password = env::var("BB_CLI_PASSWORD").unwrap();
 
-        let url = format!("{}://{}:{}@api.{}/2.0/repositories{}/",
+        let url = format!("{}://{}:{}@api.{}",
                           parsed.scheme().to_owned(),
                           username,
                           password,
-                          parsed.host_str().unwrap(),
-                          &slug);
+                          parsed.host_str().unwrap());
         return Some(url);
     } else {
         return None;
     }
 }
 
-fn get_current_pr(url: String) -> std::result::Result<String, reqwest::Error> {
-    let response: PullRequestsResponse = reqwest::blocking::get(url.as_str())?.json()?;
+fn get_current_pr() -> std::result::Result<PullRequest, reqwest::Error> {
+    let current_branch = super::git::git_current_branch();
+    let remote_url = super::git::git_remote();
 
-    if response.values.len() > 0 {
-        let pr = &response.values[0];
-        let pr_link = pr.links.html.href.clone();
-        return Ok(pr_link);
+    if let Some(base_url) = get_api_base_url() {
+        if let Ok(parsed) = Url::parse(&remote_url) {
+            let slug = parsed.path().split(".git").collect::<Vec<_>>().join("");
+
+            let pr_search_api_url = base_url
+                + "/2.0/repositories"
+                + &slug
+                + "/pullrequests?q=source.branch.name~\""
+                + current_branch.trim() + "\"";
+
+            let response: PullRequestsResponse = reqwest::blocking::get(pr_search_api_url.as_str())?.json()?;
+
+            if response.values.len() > 0 {
+                let pr = response.values.first().unwrap().to_owned();
+                return Ok(pr);
+            }
+        }
     }
-    
+
+
     panic!("Failed to find the PR link")
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+fn get_current_user_prs() -> std::result::Result<Vec<PullRequest>, reqwest::Error> {
+    if let Some(base_url) = get_api_base_url() {
+        let username = env::var("BB_CLI_USERNAME").unwrap();
+        let full_url = format!("{}/2.0/pullrequests/{}?state=OPEN", base_url, username);
+
+        let response: PullRequestsResponse = reqwest::blocking::get(full_url.as_str())?.json()?;
+
+        return Ok(response.values);
+    }
+
+    panic!("Failed to find the PR link")
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct PullRequestsResponse {
     values: Vec<PullRequest>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct PullRequest {
+    title: String,
     description: String,
+    state: String,
     links: PullRequestLinks,
+    source: PullRequestBranch,
+    destination: PullRequestBranch,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct PullRequestLinks {
     #[serde(rename = "self")]
     self_link: Link,
     html: Link,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Link {
     href: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Branch {
+    name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Repository {
+    full_name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct PullRequestBranch {
+    branch: Branch,
+    repository: Repository,
+
 }
 
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,40 @@ fn main() {
     let matches = App::new("bb-cli")
         .subcommand(
             App::new("pr")
-                .about("Pull requests commands")
+                .about("Pull request commands")
                 .subcommands(
                     vec![
-                        SubCommand::with_name("open").about("Opens the pull request in the browser"),
+                        SubCommand::with_name("view").about("Opens the pull request in the browser"),
                         SubCommand::with_name("status").about("Displays the status of the pull request"),
                         SubCommand::with_name("create").about("Opens create pull request screen in the browser"),
+                        SubCommand::with_name("list").about("List pull requests authored by you"),
+                    ]
+                )
+        )
+        .subcommand(
+            App::new("branch")
+                .about("Branch commands")
+                .subcommands(
+                    vec![
+                        SubCommand::with_name("view").about("Opens the branch in the browser"),
+                    ]
+                )
+        )
+        .subcommand(
+            App::new("commit")
+                .about("Commit commands")
+                .subcommands(
+                    vec![
+                        SubCommand::with_name("view").about("Opens the commit in the browser"),
+                    ]
+                )
+        )
+        .subcommand(
+            App::new("repo")
+                .about("Repo commands")
+                .subcommands(
+                    vec![
+                        SubCommand::with_name("view").about("Opens the repo in the browser"),
                     ]
                 )
         )
@@ -21,9 +49,10 @@ fn main() {
 
     if let Some(ref prMatches) = matches.subcommand_matches("pr") {
         match prMatches.subcommand_name() {
-            Some("open") => open_pr(),
+            Some("view") => open_pr(),
             Some("status") => status_pr(),
             Some("create") => create_pr(),
+            Some("list") => list_pr(),
             _ => {}
         };
     }
